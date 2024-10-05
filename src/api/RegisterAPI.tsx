dcca88c2f91fb8079febc29103cfcf37a8285afe
sieon <sieon.eve@gmail.com>
2024-10-06 05:29:07 +0900

    졸리다

--- src/api/RegisterAPI.tsx
+++ src/api/RegisterAPI.tsx
@@ -5,7 +5,9 @@ const apiUrl = process.env.REACT_APP_API_URL;
 export const RegisterAPI = async (userInfo: object) => {
   console.log(apiUrl);
   try {
-    const response = await axios.post(`${apiUrl}/api/user/register`, userInfo);
+    const response = await axios.post(`${apiUrl}/api/user/register`, userInfo, {
+      withCredentials: true,
+    });
     return response.data; // 로그인 성공 시 데이터 반환
   } catch (error) {
     console.error('로그인 실패:', error);
--- src/api/UserInfoAPI.tsx
+++ src/api/UserInfoAPI.tsx
@@ -4,7 +4,9 @@ const apiUrl = process.env.REACT_APP_API_URL;
 // 유저 정보 불러오는 API 함수
 export const UserInfoAPI = async (id: any) => {
   try {
-    const response = await axios.get(`${apiUrl}/api/user/${id}`);
+    const response = await axios.get(`${apiUrl}/api/user/${id}`, {
+      withCredentials: true,
+    });
     return response.data; // 성공 시 데이터 반환
   } catch (error) {
     console.error('데이터 불러오기 실패 : ', error);
