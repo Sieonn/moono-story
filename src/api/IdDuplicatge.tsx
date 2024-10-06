6840fc57abed1d1a84f4c297cdebda90ee46e0b1
sieon <sieon.eve@gmail.com>
2024-10-06 15:35:17 +0900

    try2

--- src/api/IdDuplicatge.tsx
+++ src/api/IdDuplicatge.tsx
@@ -4,13 +4,9 @@ const apiUrl = process.env.REACT_APP_API_URL;
 // 회원가입 아이디 붕복 체크 API
 export const IdDuplicateAPI = async (userInfo: { id: string }) => {
   try {
-    // const response = await axios.get(`${apiUrl}/api/user/check-id`, {
-    const response = await axios.get(
-      `https://moonostory.store/api/user/check-id`,
-      {
-        params: { id: userInfo.id }, // 쿼리 스트링으로 아이디 전달
-      }
-    );
+    const response = await axios.get(`${apiUrl}/api/user/check-id`, {
+      params: { id: userInfo.id }, // 쿼리 스트링으로 아이디 전달
+    });
     return response.data; // 성공 시 데이터 반환
   } catch (error) {
     console.error('아이디 중복입니다.', error);
